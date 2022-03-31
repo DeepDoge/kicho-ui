0399f2d2ee87ed58d1f5162430c1321eea40846d
Shiba <44804845+DeepDoge@users.noreply.github.com>
2022-03-31 23:03:54 +0000

    ...

--- Sheet.svelte
+++ Sheet.svelte
@@ -1,28 +1,3 @@
-<div class="sheet">
+<div class="g-sheet">
     <slot />
 </div>
-
-<style>
-    .sheet {
-        width: 100%;
-        height: 100%;
-        position: relative;
-        color: var(--g-foreground-contrast);
-        isolation: isolate;
-
-        transition: var(--g-transition);
-        transition-property: color;
-    }
-
-    .sheet::before {
-        content: "";
-        position: absolute;
-        inset: 0;
-        background: var(--g-foreground);
-        opacity: var(--g-foreground-opacity);
-        z-index: -1;
-
-        transition: var(--g-transition);
-        transition-property: opacity;
-    }
-</style>
