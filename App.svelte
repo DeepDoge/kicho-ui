5f90e69110fbc46d58944e9041ee3d0b64eaadcf
Shiba <44804845+DeepDoge@users.noreply.github.com>
2022-04-01 17:03:33 +0000

    half decend

--- App.svelte
+++ App.svelte
@@ -1,25 +1,52 @@
 <script lang="ts">
-    import Sheet from "./Sheet.svelte";
-
+    import "./style.css";
     export let colorMode: "light" | "dark";
+    export let colors: {
+        master: string;
+        slave: string;
+        dark: string;
+        light: string;
+    };
     export let theme: string;
 </script>
 
-<div id="g-app" class="g-color-mode-{colorMode}">
-    <Sheet {theme}>
-        <div id="g-app-content">
-            <slot />
-        </div>
-    </Sheet>
+<div
+    id="g-app"
+    class="g-color-mode-{colorMode} g-theme-{theme}"
+    style={Object.entries(colors)
+        .map(([key, value]) => `--g-color-${key}:${value}`)
+        .join(";")}
+>
+    <div id="g-app-content">
+        <slot />
+    </div>
 </div>
 
 <style>
+    #g-app-content {
+        isolation: isolate;
+        background-color: var(--g-color-mode);
+        color: var(--g-current-color);
+    }
+
     #g-app-content::before {
         content: "";
         position: fixed;
         inset: 0;
-        background-color: var(--g-color-mode);
-        opacity: .5;
+        background: var(--g-current-background);
+        opacity: 0.3;
         z-index: -1;
+        filter: blur(5em);
+        transform: scale(1.2);
+    }
+
+    :global(#g-app) {
+        font-size: max(1rem, min(1vw, 2vh));
+        font-family: Arial, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Open Sans, Helvetica Neue, sans-serif;
+
+        --g-transition: linear 0.2s;
+        --g-border-width: 0.2em;
+        --g-border-radius: 0.25em;
+        --g-color-wild: linear-gradient(120deg, var(--g-color-master), var(--g-color-slave));
     }
 </style>
--- Sheet.svelte
+++ Sheet.svelte
@@ -1,21 +1,21 @@
+<script context="module" lang="ts">
+    export type Colors = 'mono' | 'gradient' | 'master' | 'slave'
+</script>
+
 <script lang="ts">
     export let theme: string = null;
     export let child = false;
 
-    export let color: string = null
-    export let background: string = null
-
-    $: style = [
-        color && `--color:${color}`,
-        background && `--background:${background}`
-    ].filter((x) => x).join(";");
+    export let border = false;
+    export let glow = false;
+    export let rounded = false;
 
     $: classes = [theme && `g-theme-${theme}`].filter((x) => x).join(" ");
     let className = null;
     export { className as class };
 </script>
 
-<div class="sheet {className ?? ''} {classes}" class:child {style}>
+<div class="sheet {className ?? ''} {classes}" class:child class:rounded class:border class:glow>
     <div class="content">
         <slot />
     </div>
@@ -28,43 +28,56 @@
         height: 100%;
     }
 
-    .sheet:not(.child) {
-        --background: var(--g-current-background);
-        --color: var(--g-current-color);
-        --child-color: var(--g-current-child-color);
-        --child-background: var(--g-current-child-background);
-    }
-    .sheet.child {
-        --background: var(--g-current-child-background);
-        --color: var(--g-current-child-color);
-        --child-color: var(--g-current-color);
-        --child-background: var(--g-current-background);
+    .sheet {
+        position: relative;
     }
 
     .sheet {
-        position: relative;
-        isolation: isolate;
-    }
+        --background: var(--g-current-background);
+        --color: var(--g-current-color);
+        --border: var(--g-current-border-color);
+        --glow: var(--g-current-glow-color);
 
-    .sheet::before {
-        content: "";
-        position: absolute;
-        inset: 0;
-        background: var(--background);
-        z-index: -1;
-
-        opacity: 0.95;
+        --child-color: var(--g-current-child-color);
+        --child-background: var(--g-current-child-background);
+        --child-border: var(--g-current-child-border-color);
+        --child-glow: var(--g-current-child-glow-color);
     }
 
     .content {
         --g-current-color: var(--child-color);
         --g-current-background: var(--child-background);
+        --g-current-border-color: var(--child-border);
+        --g-current-glow-color: var(--child-glow);
+
         --g-current-child-background: var(--background);
         --g-current-child-color: var(--color);
+        --g-current-child-border-color: var(--border);
+        --g-current-child-glow-color: var(--glow);
+    }
 
-        color: transparent;
-        background: var(--color);
-        background-clip: text;
-        -webkit-background-clip: text;
+    .content {
+        color: var(--color);
+        background: var(--background);
+    }
+
+    .border.sheet {
+        padding: var(--g-border-width);
+        background: var(--border);
+    }
+
+    .border.glow::before {
+        content: "";
+        position: absolute;
+        inset: 0;
+        background: var(--glow);
+        z-index: -1;
+        filter: blur(2.5em);
+        transform: scale(.95);
+    }
+
+    .sheet.rounded.border,
+    .sheet.rounded:not(.border) .content {
+        border-radius: var(--g-border-radius);
     }
 </style>
