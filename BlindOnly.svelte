39ef53919fbcb50fa6da35ceec3b11012f64c042
Shiba <44804845+DeepDoge@users.noreply.github.com>
2022-04-27 22:32:10 +0000

    ...

---
+++ BlindOnly.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    export let id: string = null;
+</script>
+
+<div {id} class="blind-only" tabindex="0">
+    <slot />
+</div>
+
+<style>
+    .blind-only {
+        position: absolute;
+        background: var(--h-color-mode);
+        outline: solid 0.1em var(--h-color-master);
+        z-index: 999999999999;
+    }
+
+    .blind-only {
+        pointer-events: none;
+        opacity: 0;
+    }
+
+    .blind-only:focus,
+    .blind-only:focus-within {
+        pointer-events: unset;
+        opacity: unset;
+    }
+</style>
---
+++ BlindOnlyLink.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    export let href: string;
+    export let id: string = null;
+</script>
+
+<a {href} {id} class="blind-only-link" tabindex="0">
+    <slot />
+</a>
+
+<style>
+    .blind-only-link {
+        position: absolute;
+        background: var(--h-color-mode);
+        outline: solid 0.1em var(--h-color-master);
+        z-index: 999999999999;
+    }
+
+    .blind-only-link {
+        pointer-events: none;
+        opacity: 0;
+    }
+
+    .blind-only-link:focus,
+    .blind-only-link:focus-within {
+        pointer-events: unset;
+        opacity: unset;
+    }
+</style>
