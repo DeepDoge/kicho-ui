f81398d042975a5228ed3ff87b2926b4ce9ff934
Shiba <44804845+DeepDoge@users.noreply.github.com>
2022-05-03 21:24:06 +0000

    cool but not gonna work well

--- KOptions.svelte
+++ KOptions.svelte
@@ -5,72 +5,12 @@
 
 <script lang="ts">
     export let template: KBoxTemplate = "filled";
-    export let textFilled = false;
 </script>
 
-<div class="kicho-box-options {template}" class:text-filled={textFilled} />
+<div class="kicho-box-options {template}" />
 
 <style>
     .kicho-box-options {
         display: contents;
-    }
-
-    .kicho-box-options ~ :global(*) {
-        --k-box-border-radius: 0.5em;
-        --k-box-default-background: var(--k-color-gradient);
-        --k-box-text-color: var(--k-color-gradient-contrast);
-    }
-
-    .kicho-box-options ~ :global(*) {
-        --k-box-glow-blur: 0.25em;
-        --k-box-glow-brightness: 1;
-        --k-box-glow-background: var(--k-box-default-background);
-    }
-
-    .kicho-box-options ~ :global(*) {
-        --k-box-background: var(--k-box-default-background);
-        --k-box-foreground: var(--k-box-default-background);
-        --k-box-foreground-offset: 0.2em;
-    }
-
-    .kicho-box-options ~ :global(*) {
-        --k-box-background-opacity: 0.95;
-        --k-box-foreground-opacity: 0.95;
-        --k-box-glow-opacity: 0.5;
-    }
-
-    .kicho-box-options.filled ~ :global(*) {
-        --k-box-foreground-opacity: 0;
-    }
-
-    .kicho-box-options.outlined ~ :global(*) {
-        --k-box-foreground: var(--k-color-mode);
-        --k-box-text-color: var(--k-color-mode-inverse);
-    }
-
-    .kicho-box-options.text-filled ~ :global(*) {
-        --k-box-text-color: var(--k-box-default-background);
-    }
-    .kicho-box-options.text-filled ~ :global(*) :global(.kicho-effect:last-of-type ~ *:not(.kicho-box-options)) {
-        display: inline-block;
-        background: var(--k-box-text-color);
-        background-clip: text;
-        -webkit-background-clip: text;
-        color: transparent;
-    }
-
-    .kicho-box-options.glow-only ~ :global(*) {
-        --k-box-background: var(--k-color-mode);
-        --k-box-foreground-opacity: 0;
-        --k-box-foreground-offset: 0em;
-        --k-box-text-color: var(--k-color-mode-inverse);
-    }
-
-    .kicho-box-options.text ~ :global(*) {
-        --k-box-default-background: currentColor;
-        --k-box-background-opacity: 0;
-        --k-box-foreground-opacity: 0;
-        --k-box-glow-opacity: 0;
-        --k-box-foreground-offset: 0em;
     }
 </style>
--- components/effects/KBoxEffects.svelte
+++ components/effects/KBoxEffects.svelte
@@ -1,52 +1,55 @@
-<script lang="ts">
-</script>
-
 <div class="kicho-effect glow" />
 <div class="kicho-effect background" />
-<div class="kicho-effect foreground" />
+<div class="kicho-effect border" />
 
 <style>
     .kicho-effect {
         animation: var(--k-gradient-animation);
-        transition: var(--k-transition);
-        transition-property: filter, border-radius, background-color, inset;
+    }
+
+    .kicho-effect,
+    .glow::before {
+        position: absolute;
+        box-sizing: content-box;
+        width: 100%;
+        height: 100%;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        border: solid 0 transparent;
+        border-width: inherit;
+        border-radius: inherit;
+        background-origin: border-box;
     }
 
     .glow {
-        position: absolute;
-        inset: 0;
-        background: var(--k-box-glow-background);
-        filter: blur(var(--k-box-glow-blur)) brightness(var(--k-box-glow-brightness)) opacity(var(--k-box-glow-opacity));
-        border-radius: var(--k-box-border-radius);
+        background-image: inherit;
+        background-color: inherit;
+        filter: blur(.25em)
+    }
+
+    .glow::before {
+        content: "";
+        background-image: var(--border-color);
+        background-color: var(--border-color);
+        mask: linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0);
+        -webkit-mask: linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0);
+        -webkit-mask-composite: xor;
+        mask-composite: exclude;
+    }
+
+    .border {
+        background-image: var(--border-color);
+        background-color: var(--border-color);
+        mask: linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0);
+        -webkit-mask: linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0);
+        -webkit-mask-composite: xor;
+        mask-composite: exclude;
     }
 
     .background {
-        position: absolute;
-        inset: 0;
-        border-radius: var(--k-box-border-radius);
-        background: var(--k-box-background);
-        filter: opacity(var(--k-box-background-opacity));
-    }
-
-    .foreground {
-        position: absolute;
-        inset: var(--k-box-foreground-offset);
-        background: var(--k-box-foreground);
-        border-radius: calc(var(--k-box-border-radius) * 0.75);
-        filter: opacity(var(--k-box-foreground-opacity));
-    }
-
-    /* content */
-    :global(.kicho-effect ~ *:not(.kicho-effect)) {
-        --border: solid transparent var(--k-box-foreground-offset);
-        border-left: var(--border);
-        border-right: var(--border);
-        color: var(--k-box-text-color);
-    }
-    :global(.kicho-effect + *:not(.kicho-effect)) {
-        border-top: var(--border);
-    }
-    :global(.kicho-effect ~ *:not(.kicho-effect):last-child) {
-        border-bottom: var(--border);
+        background: inherit;
+        background-origin: padding-box;
+        transform: translate(-50%, -50%) scale(.988);
     }
 </style>
