691024467269b1cf8fe35c99a02b5b9aca0034f8
Shiba <44804845+DeepDoge@users.noreply.github.com>
2022-05-11 13:10:14 +0000

    ...

---
+++ components/effects/KLoadingEffect.svelte
@@ -0,0 +1,36 @@
+<div class="loading-blur kicho-effect" />
+<div class="loading kicho-effect" />
+
+<style>
+    .loading {
+        position: absolute;
+        inset: 0;
+        overflow: hidden;
+    }
+
+    .loading::before {
+        content: "";
+        position: absolute;
+        inset: 0;
+        background-image: linear-gradient(90deg, transparent, currentColor, transparent);
+        opacity: 0.1;
+        animation: loading infinite 1s linear;
+        filter: blur(.5rem);
+    }
+
+    @keyframes loading {
+        from {
+            transform: translateX(-100%);
+        }
+        to {
+            transform: translateX(100%);
+        }
+    }
+
+    .loading-blur {
+        display: none;
+        position: absolute;
+        inset: 0;
+        backdrop-filter: blur(.25rem);
+    }
+</style>
