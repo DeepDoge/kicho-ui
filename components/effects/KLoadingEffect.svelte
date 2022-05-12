d021034d8d7235884e0bb8452c389809aaf5e924
Shiba <44804845+DeepDoge@users.noreply.github.com>
2022-05-12 08:43:02 +0000

    ...

--- components/effects/KLoadingEffect.svelte
+++ components/effects/KLoadingEffect.svelte
@@ -11,7 +11,7 @@
         content: "";
         position: absolute;
         inset: 0;
-        background-image: linear-gradient(90deg, transparent, currentColor, transparent);
+        background-image: linear-gradient(120deg, transparent, currentColor, transparent);
         opacity: 0.5;
         animation: loading infinite 1s linear;
         filter: blur(.5rem);
