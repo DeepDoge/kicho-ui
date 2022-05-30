aab3b02676ee5c53b47a1f79c3f44a050289da4f
Shiba <44804845+DeepDoge@users.noreply.github.com>
2022-05-30 13:47:06 +0000

    🍙 added hover menu

---
+++ components/KHoverMenu.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import KBoxEffect from "./effects/KBoxEffect.svelte";
+
+    type BoxProps = KBoxEffect["$$prop_def"];
+    interface $$Props extends BoxProps {}
+
+    export let color: $$Props["color"] = "mode";
+    export let glow: $$Props["glow"] = true;
+    export let background: $$Props["background"] = true;
+</script>
+
+<div class="menu">
+    <KBoxEffect {...$$props} {color} {glow} {background}>
+        <div class="content">
+            <slot />
+        </div>
+    </KBoxEffect>
+</div>
+
+<style>
+    :global(*:not(:hover)) > .menu:not(:focus):not(:focus-within) {
+        opacity: 0;
+        pointer-events: none;
+    }
+
+    .menu {
+        position: absolute;
+        left: 0;
+        min-width: 100%;
+        top: 100%;
+
+        padding: var(--k-padding);
+
+        transition: var(--k-transition);
+        transition-property: opacity;
+        z-index: 10;
+    }
+</style>
